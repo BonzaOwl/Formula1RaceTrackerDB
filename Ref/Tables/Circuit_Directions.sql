d542f6df6be194a7247b36476aec00a1085f70ad
Bonza Owl <bonzaowl@gmail.com>
2020-01-21 07:20:19 +0000

    Added additional referential data.

---
+++ Ref/Tables/Circuit_Directions.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE Ref.Circuit_Directions
+(
+CircuitDirection_ID INT IDENTITY(1,1) PRIMARY KEY,
+Circuit_Direction nvarchar(20),
+Active BIT
+)
---
+++ Ref/Tables/Circuit_Types.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE Ref.Circuit_Types
+(
+CircuitType_ID INT IDENTITY(1,1) PRIMARY KEY,
+Circuit_Type nvarchar(20),
+Active BIT
+)
